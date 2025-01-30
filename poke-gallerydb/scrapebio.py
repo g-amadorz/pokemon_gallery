925335ef48f4d33652e970949c05a2e461c78579
g-amadorz <gabe.amadorz@gmail.com>
2025-01-30 11:42:15 -0800

    Scraped majority of the data I needed, some issues arose with the data surrounding the bio, wull have to most likely implement a try-catch function to scrape those bios

--- poke-gallerydb/scrapebio.py
+++ poke-gallerydb/scrapebio.py
@@ -15,30 +15,98 @@ class PokemonBio:
     def toDict(self):
         return {
             'pokemon': self.pokemon,
-            'name': self.bio,
+            'bio': self.bio,
             'ability': self.ability,
             'region': self.region
         }
 
+def untangle(cell):
+    if len(cell) == 1:
+        cell = [cell[0].text]
+    else:
+        cell = [makeNice(cell[0].text), makeNice(onlyOne(cell[1].text))]
+        if cell[1] == None:
+            cell = [cell[0]]
+    return cell
+
+def oddCases(pokemonName):
+    if ("Nidoran♀" == pokemonName):
+        pokemonName = 'Nidoran-f'
+    elif ('Nidoran♂' == pokemonName):
+        pokemonName = 'Nidoran-m'
+    elif ("Farfetch'd" == pokemonName):
+        pokemonName = 'Farfetchd'
+    elif('Mr. Mime' == pokemonName):
+        pokemonName = 'mr-mime'
+    return pokemonName
+
+def onlyOne(ability):
+    if ('#' in ability):
+        ability = None
+    return ability
+
+def makeNice(ability):
+    if ability == None:
+        return
+    ability = ability.replace("1.", "").replace("2.","").strip()
+    return ability
+
+        
 def getPokemonBio():
     PokemonBios = []
     for pokemon in p:
-        url = f"https://pokemondb.net/pokedex/{pokemon.name}"
+        pokemonName = oddCases(pokemon.get('name'))
+        url = f"https://pokemondb.net/pokedex/{pokemonName}"
         response = requests.get(url)
         soup = bs(response.text, 'lxml')
-        rows = soup.find_all('div', class_='grid-col span-md-6 span-lg-4 text-center')
 
-        print(rows)
+        #Abilities
+        ability_cell = soup.find_all('span', class_='text-muted')
+        ability = untangle(ability_cell)
+        ability_text = onlyOne(ability)
 
-        #PokemonBios.append(PokemonBio().toDict())
-
-
-    #return PokemonBios
-
-
-
+        #Bio
+        species_cell = soup.find('table', class_='vitals-table',).find_all('td', limit=3)
+        species_text = species_cell[2].text
 
 
+        #Region
+        region = 'Kanto'
 
+        PokemonBios.append(PokemonBio(pokemonName, species_text, ability_text, region).toDict())
+
+
+    return PokemonBios
+
+PokemonsData = getPokemonBio()
+
+try:
+    connection = psycopg2.connect(
+        dbname="pokedb",
+        user="postgres",
+        password="docker",
+        host="localhost",
+        port="5432"
+    )
+    cursor = connection.cursor()
+
+    # Insert data into table
+    insert_query = ''' 
+    INSERT INTO gallery_pokemon (num, name, type, image) VALUES (%s, %s, %s, %s)
+    '''
+
+    for pokemon in PokemonsData:
+        types_str = ', '.join(pokemon['ability'])
+        cursor.execute(insert_query, (pokemon['num'], pokemon['name'], types_str, pokemon['sprite']))
+    
+    connection.commit()
+    print("Data inserted successfully!")
+
+except Exception as error:
+    print(f"Error: {error}")
+
+finally:
+    cursor.close()
+    connection.close()
 
 
